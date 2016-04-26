c2a14853f9bfcc8626fa70b34cb549998b07c8ad
Jason Unger <jasonscottunger@gmail.com>
2016-04-26 15:22:22 -0400

    hi mom

--- hide.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-# This hides all files and folders
-# preceeded by a . in terminal
-
-hide(){ defaults write com.apple.finder AppleShowAllFiles NO && killall Finder; };
--- show.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-# This hides all files and folders
-# preceeded by a . in terminal
-
-show(){ defaults write com.apple.finder AppleShowAllFiles YES && killall Finder; };
